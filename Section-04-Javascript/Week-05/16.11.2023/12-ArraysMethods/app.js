8363e5cb1e6434a386639ce48d25ad50616f2a56
bbaransel <baran9932@gmail.com>
2023-11-16 21:51:30 +0300

    Arrays and Objects

---
+++ Section-04-Javascript/Week-05/16.11.2023/12-ArraysMethods/app.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let students = ["ayşen", "umay", "ceyda", "begüm", "köfte", "heda"];
+
+let result;
+
+result = students.length;
+result = students;
+result = students.toString();
+result = students.join(";");
+result = result.split(";");
+let products1 = ["ürün1", "ürün2", "ürün3"];
+let products2 = ["ürün4", "ürün5", "ürün6"];
+result = products1.concat(products2);
+
+// students.push("zeynep"); // Sona eleman ekler.
+// students.pop(); // Son elemanı siler.
+// students.shift(); // İlk elemanı siler.
+// students.unshift("ece"); // Dizinin başına eleman ekler.
+
+// students dizisindeki ceyda değerini taşıyan elemanın içeriğini ece olarak değiştir.
+
+// students[students.indexOf("ceyda")] = "ece";
+students.splice(students.indexOf("ceyda"), 1)
+// result = students.splice(2,3);
+
+result = students;
+
+
+
+console.log(result);
---
+++ Section-04-Javascript/Week-05/16.11.2023/13-Object/app.js
@@ -0,0 +1,55 @@
+// JavaScript Object Notation - JSON
+"use strict"
+
+// let student1 = ["Zeynep Nur", "Ay", 28, "Kadın",];
+// let student2 = ["Çağdaş", "Taş", 28, "Erkek"];
+// let student3 = ["Furkan", "Gültekin", 20, "Erkek"];
+
+// let students = [student1, student2, student3];
+// console.log(students);
+
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i][0] + " " + students[i][2]);
+// };
+
+// Yukarıdaki student1 değişkeni bir objectir. firstName, lastName, age, gender bu objectin özellikleridir.(property)
+
+// console.log(student1.firstName);
+// console.log(student1.gender + " " + student1.age);
+
+let student1 = {
+    firstName: "Zeynep Nur",
+    lastName: "Ay",
+    age: 28,
+    gender: "Kadın"
+};
+let student2 = {
+    firstName: "Mehmet Emir",
+    lastName: "Sürmeli",
+    age: 21,
+    gender: "Erkek"
+};
+let student3 = {
+    firstName: "Didier",
+    lastName: "Drogba",
+    age: 45,
+    gender: "Erkek"
+};
+
+// let students = [student1, student2, student3];
+// let total = 0;
+// for (let i = 0; i < students.length; i++) {
+//     console.log(students[i].firstName + " " + students[i].gender);
+//     total += students[i].age;
+// }
+// console.log("Bu sınıfın yaş ortalaması: " + (total / students.length));
+
+let students = [student1, student2, student3];
+let total = 0;
+let studentsCount = students.length;
+for (let i = 0; i < studentsCount; i++) {
+    console.log(students[i].firstName + " " + students[i].gender);
+    total += students[i].age;
+}
+let average = total / studentsCount;
+console.log("Bu sınıfın yaş ortalaması: " + average.toFixed(2));
