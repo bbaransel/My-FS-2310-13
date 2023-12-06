b419f98b880301f349b80ea9e5592f246e92317c
bbaransel <baran9932@gmail.com>
2023-12-06 21:57:25 +0300

    react-router started

---
+++ Section-05-ModernJavascriptReact/Week-08/06.12.2023/github-finder/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ Section-05-ModernJavascriptReact/Week-08/06.12.2023/github-finder/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <>
+            <div>
+                <h1>Hoş geldiniz</h1>
+                <p>Bu uygulama ile GitHub profillerini arayabilirsiniz</p>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
