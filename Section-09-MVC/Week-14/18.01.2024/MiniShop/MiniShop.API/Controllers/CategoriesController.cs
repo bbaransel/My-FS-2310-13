77bb5c17e72a154f23815b01f596c510948b7f6d
bbaransel <baran9932@gmail.com>
2024-01-18 21:46:55 +0300

    DTO methots
    Diğer metotları kendin yapmayı dene.(GetById'den başla)

---
+++ Section-09-MVC/Week-14/18.01.2024/MiniShop/MiniShop.API/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniShop.Business.Abstract;
+
+namespace MiniShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryManager;
+
+        public CategoriesController(ICategoryService categoryManager)
+        {
+            _categoryManager = categoryManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var response =  await _categoryManager.GetAllCategoriesWithProductsAsync();
+            return null;
+        }
+    }
+}
---
+++ Section-09-MVC/Week-14/18.01.2024/MiniShop/MiniShop.API/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using MiniShop.Business.Abstract;
+using MiniShop.Business.Concrete;
+using MiniShop.Data.Abstract;
+using MiniShop.Data.Concrete.Contexts;
+using MiniShop.Data.Concrete.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<MiniShopDbContext>(options=>
+    options.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection"))
+);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//Services
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+
+//Repositories
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+
+
+
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
