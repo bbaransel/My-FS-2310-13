38675909ba6a83eb6930cf8a46febdb06372be70
bbaransel <baran9932@gmail.com>
2024-01-30 21:51:58 +0300

    Çöp kutusu, silme işlemleri
    next : "upload image"

---
+++ Section-10-API/Week-16/30.01.2024/MiniShopUI/Mappings/Admin/GeneralMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MiniShop.MVC.Areas.Admin.Models;
+
+namespace MiniShop.MVC.Mappings.Admin
+{
+    public class GeneralMappingProfile: Profile
+    {
+        GeneralMappingProfile() 
+        { 
+          CreateMap<ProductViewModel, EditProductViewModel>()
+                .ForMember(p=>p.CategoryIds, options => options.MapFrom(x=>x.Categories.Select(c=>c.Id)))
+                .ReverseMap();
+        }
+    }
+}
