bcea3ed9c57745c89461960f8bdd480028f9aa26
bbaransel <baran9932@gmail.com>
2024-01-31 20:10:55 +0300

    Image upload hariç products bitti
    Categories i kendin yap

---
+++ Section-10-API/Week-16/31.01.2024/MiniShopUI/Helpers/Jobs.cs
@@ -0,0 +1,64 @@
+﻿using System.Formats.Tar;
+
+namespace MiniShop.MVC.Helpers
+{
+    public static class Jobs
+    {
+        public static string GetUrl(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return null;
+            text = text.Trim();
+            text = text.Replace("İ", "i");
+            text = text.Replace("I", "ı");
+            text = text.Replace("ı", "ı");
+
+            text = text.ToLower();
+            text = text.Replace("ö", "o");
+            text = text.Replace("ü", "u");
+            text = text.Replace("ş", "s");
+            text = text.Replace("ç", "c");
+            text = text.Replace("ğ", "g");
+
+
+            text = text.Replace("\"", "");
+            text = text.Replace("'", "");
+            text = text.Replace("^", "");
+            text = text.Replace("#", "");
+            text = text.Replace("+", "");
+            text = text.Replace("$", "");
+            text = text.Replace("%", "");
+            text = text.Replace("½", "");
+            text = text.Replace("&", "");
+            text = text.Replace("/", "");
+            text = text.Replace("{", "");
+            text = text.Replace("}", "");
+            text = text.Replace("(", "");
+            text = text.Replace(")", "");
+            text = text.Replace("[", "");
+            text = text.Replace("]", "");
+            text = text.Replace("=", "");
+            text = text.Replace("_", "");
+            text = text.Replace("?", "");
+            text = text.Replace("*", "");
+            text = text.Replace("<", "");
+            text = text.Replace(">", "");
+            text = text.Replace("`", "");
+            text = text.Replace("~", "");
+            text = text.Replace("|", "");
+            text = text.Replace(",", "");
+            text = text.Replace(".", "");
+            text = text.Replace(":", "");
+            text = text.Replace("¨", "");
+            text = text.Replace("€", "");
+            text = text.Replace("@", "");
+            text = text.Replace("ß", "");
+            text = text.Replace("!", "");
+            text = text.Replace(";", "");
+
+            string[] splitResult = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            text = String.Join('-', splitResult);
+
+            return text;
+        }
+    }
+}
