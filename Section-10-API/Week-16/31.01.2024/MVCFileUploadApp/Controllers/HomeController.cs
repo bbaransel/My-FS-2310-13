9598e77b1ead4fa47222a5e60fa16acc0146ea16
bbaransel <baran9932@gmail.com>
2024-01-31 21:49:45 +0300

    image and file upload started
    just started, not finished
    Datatables partials changed,
    summernote fixed

---
+++ Section-10-API/Week-16/31.01.2024/MVCFileUploadApp/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCFileUploadApp.Core;
+using MVCFileUploadApp.Models;
+using System.Diagnostics;
+
+namespace MVCFileUploadApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult UploadImage(IFormFile image) 
+        {
+            if (image == null)
+            {
+                //ViewBag.Status = false;
+                ViewBag.Message = "Resimde bir sorun var lütfen tekrar deneyiniz.";
+                return View("Index");
+            }
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", image.FileName);
+            using (var stream = new FileStream(path,FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+
+
+            //ViewBag.Status = true;
+            ViewBag.Message = "Resim başarı ile yüklendi";
+            return View("Index"); 
+        }
+
+        [HttpGet]
+        public IActionResult ResimYukle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ResimYukle(IFormFile image)
+        {
+            // Burada resim yükleme için yazacağımız methodu çağıracağız.
+            var ImageUrl = Uploader.UploadImage(image);
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Section-10-API/Week-16/31.01.2024/MVCFileUploadApp/Core/Uploader.cs
@@ -0,0 +1,30 @@
+﻿namespace MVCFileUploadApp.Core
+{
+    public static class Uploader
+    {
+        public static string UploadImage(IFormFile image)
+        {
+            //dosyaismi.uzantı
+            //Örnek image ismi : ipjone12.png
+
+            //.png
+            var extension = "." + image.FileName.Split('.')[image.FileName.Split('.').Length-1];
+
+            //localhost:7700/wwwroot/images
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot","images");
+            if(!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            //asfga2G4S12FD31SG.png
+            var fileName = $"{Guid.NewGuid()}{extension}";
+            //localhost:7700/wwwroot/images/asfga2G4S12FD31SG.png
+            var path = Path.Combine(filePath,fileName);
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+            return fileName;
+        }
+    }
+}
