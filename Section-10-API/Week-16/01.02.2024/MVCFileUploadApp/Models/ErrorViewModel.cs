bb9b295fbbb8dcff8875b622685331c707ffc7c5
bbaransel <baran9932@gmail.com>
2024-02-01 21:53:08 +0300

    upload image on api
    FromForm,
    IFromFile,
    valid control,
    mvc file upload button added,
    next mvc layer file upload

---
+++ Section-10-API/Week-16/01.02.2024/MVCFileUploadApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCFileUploadApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Section-10-API/Week-16/01.02.2024/MiniShop/MiniShop.Shared/Helpers/Concrete/ImageHelper.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using MiniShop.Shared.Helpers.Abstract;
+using MiniShop.Shared.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.Helpers.Concrete
+{
+    public class ImageHelper : IImageHelper
+    {
+        //localhost:5000/wwwroot/images/
+        private readonly string _imagesFolder;
+        public ImageHelper(IWebHostEnvironment _environment)
+        {
+            _imagesFolder = Path.Combine(_environment.WebRootPath, "images");
+        }
+
+        public bool ImageIsValid(string extension)
+        {
+            string[] correctExtensions = { ".png", ".jpg", "jpeg" };
+            if(correctExtensions.Contains(extension))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile image, string folderName)
+        {
+            if (image== null)
+            {
+                return "Bir hata oluştu.";
+            }
+            var fileExtension = Path.GetExtension(image.FileName);
+            if (!ImageIsValid(fileExtension))
+            {
+                return "Hatalı resim dosyası";
+            }
+            //localhost:5000/wwwroot/images/products
+            //localhost:5000/wwwroot/images/categories
+            //localhost:5000/wwwroot/images/users
+            var targetFolder = Path.Combine(_imagesFolder, folderName);
+            if(!Directory.Exists(targetFolder))
+            {
+                Directory.CreateDirectory(targetFolder);
+            }
+            var fileName = $"{Guid.NewGuid()}{fileExtension}";
+            var fileFullPath = Path.Combine(targetFolder, fileName);
+            await using (var stream = new FileStream(fileFullPath, FileMode.Create))
+            {
+                image.CopyTo(stream);
+            }
+            return fileName;
+        }
+    }
+}
