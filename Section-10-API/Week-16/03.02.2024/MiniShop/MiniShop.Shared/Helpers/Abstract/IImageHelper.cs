7b65ba58aec9cfe7541d446feeed6fafb05c0d45
bbaransel <baran9932@gmail.com>
2024-02-03 14:45:23 +0300

    Image upload done
    next: payment methods

---
+++ Section-10-API/Week-16/03.02.2024/MiniShop/MiniShop.Shared/Helpers/Abstract/IImageHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using MiniShop.Shared.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.Helpers.Abstract
+{
+    public interface IImageHelper
+    {
+        Task<string> UploadImageAsync(IFormFile image, string folderName);
+        bool ImageIsValid(string extension);
+    }
+}
