c080ba6c4842bb27b4540c5633c550ccaff761cb
bbaransel <baran9932@gmail.com>
2024-02-15 18:50:24 +0300

    dosya düzenleme

---
+++ Section-11-Identity/Week-18/15.02.2024/MiniShop/MiniShop.Shared/ViewModels/IdentityModels/UserRolesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ViewModels.IdentityModels
+{
+    public class UserRolesViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public List<AssignRoleViewModel> Roles { get; set; }
+    }
+}
