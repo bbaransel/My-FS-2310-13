654b0cef6c93bc264ffccb61bc0993e2fd5f81e6
bbaransel <baran9932@gmail.com>
2024-02-20 18:54:50 +0300

    dosya düzeni

---
+++ Section-11-Identity/Week-19/15.02.2024/MiniShop/MiniShop.Shared/ViewModels/ShoppingCartItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ViewModels
+{
+    public class ShoppingCartItemViewModel
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int Quantity { get; set; }
+        public int ShoppingCartId { get; set; }
+        public ShoppingCartViewModel ShoppingCart { get; set; }
+    }
+}
---
+++ Section-11-Identity/Week-19/15.02.2024/MiniShop/MiniShop.UI/Controllers/ShoppingCartController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiniShop.Business.Abstract;
+using MiniShop.Entity.Concrete.Identity;
+using MiniShop.Shared.ViewModels;
+
+namespace MiniShop.UI.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IShoppingCartService _shoppingCartManager;
+        private readonly IShoppingCartItemService _shoppingCartItemService;
+
+        public ShoppingCartController(UserManager<User> userManager, IShoppingCartService shoppingCartManager, IShoppingCartItemService shoppingCartItemService)
+        {
+            _userManager = userManager;
+            _shoppingCartManager = shoppingCartManager;
+            _shoppingCartItemService = shoppingCartItemService;
+        }
+
+        // Kullanıcının sepetini gösterecek
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var shoppingCart = await _shoppingCartManager.GetShoppingCartByUserIdAsync(userId);
+            return View(shoppingCart.Data);
+        }
+        public async Task<IActionResult> AddToCart(int productId, int quantity = 1)
+        {
+            var userId =  _userManager.GetUserId(User);
+            await _shoppingCartManager.AddToCartAsync(userId, productId, quantity);
+            return Redirect("~/");
+        }
+        public async Task<IActionResult> ChangeQuantity(ShoppingCartItemViewModel shoppingCartItemViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                await _shoppingCartItemService.ChangeQuantityAsync(shoppingCartItemViewModel.Id, shoppingCartItemViewModel.Quantity);
+                return RedirectToAction("Index");
+            }
+            return View(shoppingCartItemViewModel);
+        }
+    }
+}
