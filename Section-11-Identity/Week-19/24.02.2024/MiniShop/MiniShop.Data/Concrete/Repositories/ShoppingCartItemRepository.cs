734bbccca8abaed648f851404b37ca87c13f2f5c
bbaransel <baran9932@gmail.com>
2024-02-24 14:44:48 +0300

    admin panel siparişlerim devam
    siparişlerimde ürüne göre listeleme yapılacak,
    Ajaxa başlanacak

---
+++ Section-11-Identity/Week-19/24.02.2024/MiniShop/MiniShop.Data/Concrete/Repositories/ShoppingCartItemRepository.cs
@@ -0,0 +1,26 @@
+﻿using MiniShop.Data.Abstract;
+using MiniShop.Data.Concrete.Contexts;
+using MiniShop.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data.Concrete.Repositories
+{
+    public class ShoppingCartItemRepository : GenericRepository<ShoppingCartItem>, IShoppingCartItemRepository
+    {
+        public ShoppingCartItemRepository(MiniShopDbContext _context) : base(_context) { }
+        private MiniShopDbContext MiniShopDbContext
+        {
+            get { return _dbContext as MiniShopDbContext; }
+        }
+        public async Task ChangeQuantityAsync(ShoppingCartItem shoppingCartItem, int quantity)
+        {
+            shoppingCartItem.Quantity = quantity;
+            MiniShopDbContext.Update(shoppingCartItem);
+            await MiniShopDbContext.SaveChangesAsync();
+        }
+    }
+}
