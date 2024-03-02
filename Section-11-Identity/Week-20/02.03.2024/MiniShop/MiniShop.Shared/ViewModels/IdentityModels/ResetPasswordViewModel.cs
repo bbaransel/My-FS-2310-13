483c9644d3b1adf678521d6232678027462de448
bbaransel <baran9932@gmail.com>
2024-03-02 14:54:10 +0300

    forgot password action
    forgot and reset password,
    remember me,
    toast notification,
    login action changes(needs fix)

---
+++ Section-11-Identity/Week-20/02.03.2024/MiniShop/MiniShop.Shared/ViewModels/IdentityModels/ResetPasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ViewModels.IdentityModels
+{
+    public class ResetPasswordViewModel
+    {
+        public string UserId { get; set; }
+
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage ="{0} alanı boş bırakılamaz")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Şifre Tekrar")]
+        [Required(ErrorMessage = "{0} alanı boş bırakılamaz")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage = "İki şifre birbiri ile uyuşmuyor.")]
+        public string RePassword { get; set; }
+        public string TokenCode { get; set; }
+    }
+}
