e7b8e23049a0211595282075d981bd53dfb4241b
bbaransel <baran9932@gmail.com>
2024-03-03 14:44:38 +0300

    clearcart method with ajax
    login kısmında değişiklikler,
    clearcart with ajax,
    changequantity with ajax

---
+++ Section-11-Identity/Week-20/03.03.2024/MiniShop/MiniShop.Shared/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+        [JsonPropertyName("Name")]
+        [DisplayName("Ürün")]
+        [Required(ErrorMessage = "{0} alanı boş bırakılamaz.")]
+        [MinLength(5, ErrorMessage = "{0} alanına uzunluğu {1} karakterden küçük değer girilemez.")]
+        [MaxLength(100, ErrorMessage = "{0} alanına uzunluğu {1} karakterden büyük değer girilemez.")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("Description")]
+        [DisplayName("Kategori Açıklaması")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("Url")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("IsActive")]
+        [DisplayName("Aktif Kategori")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Section-11-Identity/Week-20/03.03.2024/MiniShop/MiniShop.Shared/ViewModels/IdentityModels/UserProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ViewModels.IdentityModels
+{
+    public class UserProfileViewModel
+    {
+        public UserViewModel User { get; set; }
+        public List<AdminOrderViewModel> Orders { get; set; }
+    }
+}
